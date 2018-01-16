6d791eccb76f2a29c306450df8e370b08b278acd
Anmol varma <anmol_varma@github.com>
2018-01-16 10:14:03 +0530

    Higher order functions

---
+++ hof.js
@@ -0,0 +1,22 @@
+let c = 0;
+function operation(){
+	console.log("global counter value is: ", c);
+	return 1;
+}
+
+function repeat(operation, num){
+	c=0;
+	for(let i= 0; i < num; i++){
+		c++;
+		operation();
+	}
+}
+module.exports = repeat;
+
+console.log(operation());
+repeat(operation, 5);
+console.log("************");
+repeat(operation, -1);
+console.log("************");
+repeat(operation, 3);
+//console.log(c);
