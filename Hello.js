13c7d8d33f391497606ad797bd48f72f8855e1e0
Anmol varma <anmol_varma@github.com>
2018-01-16 10:05:01 +0530

    Hello exercise

---
+++ Hello.js
@@ -0,0 +1,10 @@
+function upperCase( input ){
+	//console.log(input.toUpperCase());
+	return input.toUpperCase();
+}
+//console.log(upperCase("hello."));
+//console.log(upperCase(123));;
+//console.log(upperCase(true));;
+//console.log(upperCase([1,2,3,4,5]));;
+console.log(upperCase("yo."));
+module.exports = upperCase;
