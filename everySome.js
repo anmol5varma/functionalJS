44ea8b603ade0f962ce024cfaf8f3684e611607e
Anmol varma <anmol_varma@github.com>
2018-01-16 12:03:20 +0530

    All files till now added

---
+++ everySome.js
@@ -0,0 +1,22 @@
+function checkUsersValid(goodUsers, submittedUsers){
+	return (function allUsersValid(submittedUsers){
+		return submittedUsers.every( function(users){
+			return goodUsers.some( function(user){
+				return user.id === users.id;
+			});
+		});
+	});
+}
+
+let gU = [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 }
+    ];
+let submittedUsers = [
+			{ id: 2 },
+			{ id: 1 }
+		];
+console.log(checkUsersValid(gU));
+
+module.exports = checkUsersValid;
